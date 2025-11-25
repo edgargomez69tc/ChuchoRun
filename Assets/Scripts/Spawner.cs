30c86a79e6879b65c3eb4c84dc96c22042bf5658
edgargomez69tc <edgargomez69tc@aragon.unam.mx>
2025-11-25 14:21:22 -0600

    Commit Adrian

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject [] obstaclePrefabs;
+    public float obstacleSpawnTime = 2f;
+    private float timeUntilObstacleSpawn;
+    public float obstacleSpeed = 1f;
+    public Transform player; // ← referencia al jugador
+    public float distanceAhead = 5f; // qué tan adelante del jugador debe ir
+
+    private void LateUpdate()
+    {
+        // Mover el Spawner enfrente del jugador
+        transform.position = new Vector3(
+            player.position.x + distanceAhead,
+            transform.position.y,
+            transform.position.z);
+        SpawnLoop();
+    }
+
+    private void SpawnLoop()
+    {
+        timeUntilObstacleSpawn += Time.deltaTime;
+        if (timeUntilObstacleSpawn >= obstacleSpawnTime)
+        {
+            Spawn();
+            timeUntilObstacleSpawn = 0f;
+        }
+    }
+
+    private void Spawn()
+    {
+        GameObject obstacleToSpawn = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+
+        GameObject spawnedObstacle = Instantiate(obstacleToSpawn, transform.position, Quaternion.identity);
+
+        Rigidbody2D obstacleRB = spawnedObstacle.GetComponent<Rigidbody2D>();
+        obstacleRB.velocity = Vector2.left* obstacleSpeed;
+    }
+}
